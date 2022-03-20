63da47022ed8220427a59e36e67efe08be402091
Myrian Rodrigues <myrian@icomp.ufam.edu.br>
2022-03-20 17:54:48 -0400

    commit 1

--- java-project/src/test/java/br/edu/icomp/ufam/gc/myrian/ConversorFahrenheitTest.java
+++ java-project/src/test/java/br/edu/icomp/ufam/gc/myrian/ConversorFahrenheitTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 // import org.junit.Assert.assertEquals;
 // import org.junit.Test;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConversorFahrenheitTest {
 
