8ad69abde7ca3735547390c701586be34c9bf875
Myrian Rodrigues <myrian@icomp.ufam.edu.br>
2022-03-20 14:52:27 -0400

    Commit da primeira versão

--- src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package br.edu.icomp.ufam.gc.myrian;
-
-public class Main {
-    public static void main(String[] args){
-        Conversor c = new Conversor('C', 84);
-
-        c.converter('F');
-    }
-}
