95d3d4ef7d6b08ef16d0cc221d1dd62d3859354f
Myrian Rodrigues <myrian@icomp.ufam.edu.br>
2022-03-20 14:56:36 -0400

    Commit da primeira versão

---
+++ java-project/src/main/java/br/edu/icomp/ufam/gc/myrian/Main.java
@@ -0,0 +1,9 @@
+package br.edu.icomp.ufam.gc.myrian;
+
+public class Main {
+    public static void main(String[] args){
+        Conversor c = new Conversor('C', 84);
+
+        c.converter('F');
+    }
+}
---
+++ java-project/src/test/java/br/edu/icomp/ufam/gc/myrian/ConversorFahrenheitTest.java
@@ -0,0 +1,24 @@
+package br.edu.icomp.ufam.gc.myrian;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConversorFahrenheitTest {
+
+    private Conversor conversorTest;
+
+    @BeforeEach
+    public void setup(){
+        conversorTest = new Conversor('F', 50);
+    }
+
+    @Test
+    public void testeConvertendoDeFahrenheitParaCelsius() {
+        conversorTest.converter('C');
+
+        assertEquals(conversorTest.getTemp().getEscala(), 'C');
+        assertEquals(conversorTest.getTemp().getTemperatura(), 10.00);
+    }
+}
